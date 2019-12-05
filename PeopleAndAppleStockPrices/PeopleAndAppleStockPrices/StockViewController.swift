30c623c0d5e20f86a0ac1d174f60592977d6f3ef
David <lin.david985@gmail.com>
2019-12-05 14:10:25 -0500

    updated UI

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StockViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StockViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by David Lin on 12/3/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
